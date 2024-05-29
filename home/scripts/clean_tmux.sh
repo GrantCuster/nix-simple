4e4b17dc47ae93f107f3e4525b55b1f62391ff1f
Grant Custer <grantcuster@gmail.com>
2024-05-29 01:03:11 +0000

    fix tmux clean

--- home/scripts/clean_tmux.sh
+++ home/scripts/clean_tmux.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-for session in printf '%s\n' "$(tmux list-sessions -F "#{session_name}:#{session_attached}")"; do
+for session in $(printf '%s\n' "$(tmux list-sessions -F "#{session_name}:#{session_attached}")"); do
   session_name=$(echo $session | cut -d: -f1)
   session_attached=$(echo $session | cut -d: -f2)
+  echo $session_name
   if [[ $session_name != \** ]] && [[ $session_attached == 0 ]]; then
     tmux kill-session -t "$session_name"
   fi
