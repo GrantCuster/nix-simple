a8569ed3c3b25ba11066d449bdcdb0276f07856f
Grant Custer <grantcuster@gmail.com>
2024-06-04 00:51:01 +0000

    fix session

--- home/scripts/welcome.sh
+++ home/scripts/welcome.sh
@@ -8,7 +8,6 @@ echo $chosen_pokemon
 
 # Check if we're inside a tmux session
 if [ -z "$TMUX" ]; then
-    echo "Not in a tmux session. Starting a new tmux session named $session_name..."
     tmux new-session -s "$chosen_pokemon" -d
     # tmux send-keys -t "$chosen_pokemon" "clear; oblique | pokemonsay --pokemon ${chosen_pokemon} --no-name" C-m
     tmux attach-session -t "$chosen_pokemon"    # Attach to the new session
