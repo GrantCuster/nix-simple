b3753073dc695cf7444ef3b5c70f62cd5f9a1073
Grant Custer <grantcuster@gmail.com>
2024-06-04 00:47:37 +0000

    modify gif

--- home/scripts/gif.sh
+++ home/scripts/gif.sh
@@ -20,8 +20,8 @@ is_recorder_running() {
 }
 
 convert_to_gif() {
-  ffmpeg -i "$TMP_MP4_FILE" -filter_complex "[0:v] palettegen" "$TMP_PALETTE_FILE"
-  ffmpeg -i "$TMP_MP4_FILE" -i "$TMP_PALETTE_FILE" -filter_complex "[0:v] fps=10,scale=1040:-1,setpts=0.666*PTS [new];[new][1:v] paletteuse" "$TMP_FILE_UNOPTIMIZED"
+  ffmpeg -i "$TMP_MP4_FILE" -filter_compjlex "[0:v] palettegen" "$TMP_PALETTE_FILE"
+  ffmpeg -i "$TMP_MP4_FILE" -i "$TMP_PALETTE_FILE" -filter_complex "[0:v] fps=10,scale=1400:-1,setpts=0.5*PTS [new];[new][1:v] paletteuse" "$TMP_FILE_UNOPTIMIZED"
   if [ -f "$TMP_PALETTE_FILE" ]; then
     rm "$TMP_PALETTE_FILE"
   fi
