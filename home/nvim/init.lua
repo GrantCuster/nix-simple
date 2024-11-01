4710dae229bdd3c48ca8d0560903af79410526a1
Grant Custer <grantcuster@gmail.com>
2024-10-31 20:07:05 -0400

    oil update

--- home/nvim/init.lua
+++ home/nvim/init.lua
@@ -335,6 +335,11 @@ require("lazy").setup({
 	{
 		"stevearc/oil.nvim",
 		opts = {
+      default_file_explorer = true,
+      skip_confirm_for_simple_edits = true,
+      view_options = {
+        show_hidden = true
+      },
 			keymaps = {
 				["g?"] = "actions.show_help",
 				["<CR>"] = "actions.select",
@@ -351,7 +356,9 @@ require("lazy").setup({
 				["<C-h>"] = false,
 				["<C-t>"] = { "actions.select", opts = { tab = true }, desc = "Open the entry in new tab" },
 				["<C-p>"] = "actions.preview",
-				["<C-c>"] = "actions.close",
+				-- ["<C-c>"] = "actions.close",
+				["<C-c>"] = false,
+				["q"] = "actions.close",
 				-- ["<C-l>"] = "actions.refresh",
 				["<C-l>"] = false,
 				["-"] = "actions.parent",
